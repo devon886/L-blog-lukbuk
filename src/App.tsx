ceff81d7c191a66854aea0fd501b8b293d83126d
lukbuk <minecraftlove1902@outlook.com>
2026-04-03 23:19:52 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,10 @@ import Header from './components/Header';
 import { AuthProvider } from './contexts/AuthContext';
 import './App.css';
 
-// 实现组件懒加载
 const HomePage = lazy(() => import('./pages/HomePage'));
 const PostDetailPage = lazy(() => import('./pages/PostDetailPage'));
 const WritePage = lazy(() => import('./pages/WritePage'));
-const AboutPage = lazy(() => import('./pages/AboutPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
-const CreateColumnPage = lazy(() => import('./pages/CreateColumnPage'));
-const ColumnDetailPage = lazy(() => import('./pages/ColumnDetailPage'));
 
 function App() {
   return (
@@ -25,9 +21,6 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/posts/:id" element={<PostDetailPage />} />
                 <Route path="/write" element={<WritePage />} />
-                <Route path="/create-column" element={<CreateColumnPage />} />
-                <Route path="/columns/:id" element={<ColumnDetailPage />} />
-                <Route path="/about" element={<AboutPage />} />
                 <Route path="/login" element={<LoginPage />} />
               </Routes>
             </Suspense>
