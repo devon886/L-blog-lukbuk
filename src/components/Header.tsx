31d97c54ceee900ecfbfff6199659dc642e21a59
lukbuk <minecraftlove1902@outlook.com>
2026-04-03 23:19:10 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,24 +15,13 @@ const Header: React.FC = () => {
             <h1 className="site-title">首页</h1>
           </Link>
           
-          <Link to="/about" className="nav-link">
-            关于
-          </Link>
-          
           <div className="nav-right">
             {CONFIG.ADMIN_ENABLED ? (
               user ? (
-                // 登录状态：显示写文章和创建专栏
-                <>
-                  <Link to="/write" className="nav-link write-link">
-                    写文章
-                  </Link>
-                  <Link to="/create-column" className="nav-link write-link">
-                    创建专栏
-                  </Link>
-                </>
+                <Link to="/write" className="nav-link write-link">
+                  写文章
+                </Link>
               ) : (
-                // 未登录状态：显示登录
                 <Link to="/login" className="nav-link">
                   登录
                 </Link>
